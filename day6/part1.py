e8f92be736d751e9a63cf1b273c8e619211b3ed0
Dan Vinegrad <dvinegrad@squarespace.com>
2022-12-09 22:32:23 -0500

    condense

--- day6/part1.py
+++ day6/part1.py
@@ -1,5 +1,4 @@
 with open('input.txt') as f:
     msg = f.read()
 
-res = next(filter(lambda i: len(set(msg[i:i+4])) == 4, range(len(msg)))) + 4
-print(res)
+print(next(filter(lambda i: len(set(msg[i:i+4])) == 4, range(len(msg)))) + 4)
