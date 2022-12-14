7a25f7e557ba4db924f385ce09e84c4d6d1b69b2
Dan Vinegrad <dvinegrad@squarespace.com>
2022-12-13 19:29:32 -0500

    day11

---
+++ day11/part1.py
@@ -0,0 +1,51 @@
+class Monkey:
+    def __init__(self, items, op, divisor, next_true, next_false):
+        self.items = items
+        self.op = op
+        self.divisor = divisor
+        self.next_true = next_true
+        self.next_false = next_false
+        self.inspected_count = 0
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def execute_round(self):
+        results = []
+        for item in self.items:
+            op_components = self.op.split(" ")
+            val = item if op_components[2] == "old" else int(op_components[2])
+            new_val = item + val if op_components[1] == "+" else item * val
+            new_val //= 3
+            new_monkey = self.next_true if new_val % self.divisor == 0 else self.next_false
+            results.append((new_val, new_monkey))
+            self.inspected_count += 1
+        
+        self.items.clear()
+        return results
+
+    def get_inspected_count(self):
+        return self.inspected_count
+
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+
+monkeys = []
+
+for i in range(0, len(lines), 7):
+    items = [int(v) for v in lines[i + 1].split(": ")[1].split(", ")]
+    op = lines[i + 2].split(" = ")[1]
+    divisor = int(lines[i + 3].split(" ")[-1])
+    next_true = int(lines[i + 4].split(" ")[-1])
+    next_false = int(lines[i + 5].split(" ")[-1])
+    monkeys.append(Monkey(items, op, divisor, next_true, next_false))
+
+for i in range(20):
+    for m in monkeys:
+        results = m.execute_round()
+        for res in results:
+            monkeys[res[1]].add_item(res[0])
+
+vals = sorted(map(lambda m: m.get_inspected_count(), monkeys), reverse=True)
+
+print(vals[0] * vals[1])
